ba2baf71c97314b9678631931edf5a5aae28d788
Phitchayut <70578824+Phitchayut@users.noreply.github.com>
2022-05-24 21:22:57 +0700

    SideBar & MainDash

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Sidebar.css'
 import Logo from '../../imgs/logo.png'
+import { SidebarData } from '../../Data/Data'
+import {UilSignOutAlt} from '@iconscout/react-unicons'
+
 
 const Sidebar = () => {
+
+  const [selected,setSelected] = useState(0)
   return (
     <div className='Sidebar'>
         {/* Logo */}
@@ -11,6 +16,28 @@ const Sidebar = () => {
             <span>
                 Sh<span>o</span>ps    
             </span>        
+        </div>
+
+        {/* menu */}
+        <div className="menu">
+           {
+             SidebarData.map((item,index) => {
+               return (
+                 <div className={selected===index ? 'menuItems active' : 'menuItems'}
+                 key={index}
+                 onClick={()=>setSelected(index)}
+                 >
+                   <div>
+                     <item.icon />
+                   </div>
+                   <span>{item.heading}</span>
+                 </div>
+               )
+             })
+           }
+           <div className="menuItems">
+           <UilSignOutAlt />
+           </div>
         </div>
     </div>
   )
